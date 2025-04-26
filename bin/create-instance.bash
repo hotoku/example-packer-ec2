2ce3b93a830e2378d4d3dbe1602681dc07ec7c3a
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2025-04-26 17:54:24 +0900

    [upd] fix

--- bin/create-instance.bash
+++ bin/create-instance.bash
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 
+
 aws ec2 run-instances \
-  --image-id "ami-014ff9dd161ad7ae6" \
-  --instance-type "c3.2xlarge" \
-  --key-name "HFT" \
-  --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"Encrypted":false,"DeleteOnTermination":true,"Iops":3000,"SnapshotId":"snap-0bfcd48f0061769cf","VolumeSize":200,"VolumeType":"gp3","Throughput":125}}' \
-  --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-02551386a99d9b334"]}' \
-  --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"temp-001"}]}' \
-  --private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
-  --count "1" \
-  --region "ap-northeast-1"
+    --image-id "ami-014ff9dd161ad7ae6" \
+    --instance-type "c3.2xlarge" \
+    --key-name "HFT" \
+    --block-device-mappings '{"DeviceName":"/dev/sda1","Ebs":{"Encrypted":false,"DeleteOnTermination":true,"Iops":3000,"VolumeSize":200,"VolumeType":"gp3","Throughput":125}}' \
+    --network-interfaces '{"AssociatePublicIpAddress":true,"DeviceIndex":0,"Groups":["sg-02551386a99d9b334"]}' \
+    --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"temp-001"}]}' \
+    --private-dns-name-options '{"HostnameType":"ip-name","EnableResourceNameDnsARecord":true,"EnableResourceNameDnsAAAARecord":false}' \
+    --count "1" \
+    --region "ap-northeast-1"
--- packer/startup.bash
+++ packer/startup.bash
@@ -2,9 +2,10 @@
 
 echo "Starting Packer build process..."
 
-apt install -y zsh jq unzip sqlite3 cmake \
+apt-get update
+apt-get install -y zsh jq unzip sqlite3 cmake \
     make build-essential libssl-dev zlib1g-dev \
     libbz2-dev libreadline-dev libsqlite3-dev wget curl \
     libncursesw5-dev xz-utils tk-dev libxml2-dev \
     libxmlsec1-dev libffi-dev liblzma-dev \
-    # direnv fzf llvm 
+    llvm direnv fzf
